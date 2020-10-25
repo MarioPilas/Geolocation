98654986c690319ce7f50d79f1e5e13f50f221d7
Mario Pilaš <mariopila123@gmail.com>
2020-10-25 22:40:15 +0100

    Geolocation
    Showing what weather is based on your location

---
+++ src/Error.js
@@ -0,0 +1,12 @@
+import './SeasonDisplay.css';
+import React from 'react';
+
+const Error = () => {
+    return (
+        <div>
+            <i className="massive thumbs down icon bgn"></i>
+        </div>
+    );
+};
+
+export default Error;
